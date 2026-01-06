8e51045fd60c7ebac996ae079b390665b8672534
njobrien1006 <59174199+njobrien1006@users.noreply.github.com>
2026-01-06 01:35:14 +0000

    Additional CodeSpace Updates

--- tests/conftest.py
+++ tests/conftest.py
@@ -20,46 +20,24 @@ def auto_enable_custom_integrations(enable_custom_integrations):
 
 
 class TraegerTestClient(Traeger):
-    def __init__(self, hass: HomeAssistant, mac_address: str) -> None:
-        super().__init__(hass, mac_address)
+    def __init__(self, username, password, hass: HomeAssistant, request_library) -> None:
+        super().__init__("johnytraeger@traeger.com", "johnytraeger'spassword", hass, request_library)
         self.published_messages: list[MQTTMessage] = []
 
-    def get_unique_id_for_state_key(self, state_key: str) -> str:
-        return f"sensor_{DOMAIN}_{self.mac_address}_{state_key}"
-
-    def publish(
-        self, topic: str, payload: str, qos: int = 1, retain: bool = True, **kwargs: Any
-    ) -> None:
-        message = MQTTMessage(topic=topic.encode("utf8"))
-        message.payload = payload.encode("utf8")
-        message.qos = qos
-        message.retain = retain
-        self.published_messages.append(message)
-
-    async def apply_state_messages(self, states: dict[str, str]) -> None:
-        for key, value in states.items():
-            message = MQTTMessage(
-                topic=f"{self._state_topic_prefix}{key}".encode("utf8")
-            )
-            message.payload = value.encode("utf8")
-            self._on_message(self._mqtt_client, None, message)
-
-        # The handlers get invoked from a thread, so they need to queue a task
-        # So wait for those
-        await self.hass.async_block_till_done()
-
-    async def connect(self) -> None: ...
-
-    async def disconnect(self) -> None:
-        self._on_disconnect()
-        await self.hass.async_block_till_done()
+    # pylint: disable=dangerous-default-value
+    async def __api_wrapper(self,
+                            method: str,
+                            url: str,
+                            data: dict = {},
+                            headers: dict = {}) -> dict:
+        url = url
 
 
 @pytest.fixture
 async def traeger_client(hass: HomeAssistant) -> TraegerTestClient:
     session = async_get_clientsession(hass)
     client = TraegerTestClient("johnytraeger@traeger.com", "johnytraeger'spassword", hass, session)
-    client.state.is_online = True
+    #client.state.is_online = True
     return client
 
 
